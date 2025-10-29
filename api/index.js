86a5457239e531be4f9928afabbcb9189df8526a
FlowEdge Solutions <info@jalud.de>
2025-10-29 18:00:20 +0100

    Fix: Express-Import fehlte - 500 Error behoben

--- api/index.js
+++ api/index.js
@@ -1,7 +1,5 @@
+// Vercel Serverless Function Handler
 const app = require('../backend/server');
 
-// Export as Vercel serverless function handler
-module.exports = async (req, res) => {
-  // Let Express handle the request
-  return app(req, res);
-};
+// Export the Express app directly for Vercel
+module.exports = app;
